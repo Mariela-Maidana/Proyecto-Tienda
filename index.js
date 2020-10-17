03299e0558e7e39c2ea62d43138d1080a03a22b3
marumaidana <maruu.mariela@gmail.com>
2020-10-17 19:36:26 -0300

    Agregado de animaciones en el maquetado

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// FILTROSSSS //
+
 const filtroBusqueda = document.querySelector("#busqueda");
 const tarjetas = document.getElementsByTagName("article");
-const filtroRating = document.getElementsByClassName("stars-input");
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+const BotonLimpiar = document.querySelector("#tacho");
+
+
 
 filtroBusqueda.oninput = () => {
-
     for (let tarjeta of tarjetas) {
         const titulo = tarjeta.dataset.nombre;
         const busqueda = filtroBusqueda.value;
@@ -17,19 +21,59 @@ filtroBusqueda.oninput = () => {
 }
 
 
-for (let checkbox of filtroRating) {
+
+for (let checkbox of checkboxes) {
     checkbox.onclick = () => {
-        for (let tarjeta of tarjetas) {
-            if (checkbox.checked) {
-                const rating = tarjeta.dataset.rating;
-                if (checkbox.value === rating) {
-                    tarjeta.classList.remove('hidden');
-                } else {
-                    tarjeta.classList.add('hidden');
-                }
-            } else {
-                tarjeta.classList.remove('hidden');
-            }
+        filtrarTarjetas();
+    };
+}
+
+
+const hayCkeckboxSeleccionado = () => {
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            return true
         }
     }
+    return false
+}
+
+
+const coincidenCheckboxYTarjeta = tarjeta => {
+    const rating = tarjeta.dataset.rating;
+    for (let checkbox of checkboxes) {
+        if (checkbox.value === rating && checkbox.checked) {
+            return true;
+        }
+
+    }
+    return false
+}
+
+const filtrarTarjetas = () => {
+    for (let tarjeta of tarjetas) {
+        tarjeta.classList.add('hidden');
+        if (hayCkeckboxSeleccionado()) {
+            if (coincidenCheckboxYTarjeta(tarjeta)) {
+                tarjeta.classList.remove('hidden');
+            }
+        } else {
+            tarjeta.classList.remove('hidden')
+        }
+    }
+}
+
+//   FIN DE FILTROSSSS   //
+
+
+
+// PARA LIMPIAR FILTROS//
+
+BotonLimpiar.onclick = () => {
+    filtroBusqueda.value = ""
+
+    for (let checkbox of checkboxes) {
+        checkbox.checked = false
+    }
+
 }
